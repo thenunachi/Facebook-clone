b629351a8f6176a8713be4caf4fc6987d5132b43
Thenu <anbunachiappan@gmail.com>
2023-03-30 17:13:17 -0700

    commented out react_root function

--- app/__init__.py
+++ app/__init__.py
@@ -85,10 +85,10 @@ def inject_csrf_token(response):
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
-    return app.send_static_file('index.html')
+# def react_root(path):
+#     if path == 'favicon.ico':
+#         return app.send_static_file('favicon.ico')
+#     return app.send_static_file('index.html')
 
 @app.route('/chat/<int:receiverId>',methods=["POST"])
 def chat(receiverId):
