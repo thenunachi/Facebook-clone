67ab5ec6fdf596267eefc1377680ad732b244b49
Thenu <anbunachiappan@gmail.com>
2023-04-03 09:47:03 -0700

    removed two lines of codes of path

--- app/__init__.py
+++ app/__init__.py
@@ -83,8 +83,8 @@ def inject_csrf_token(response):
     return response
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
 # def react_root(path):
 #     if path == 'favicon.ico':
 #         return app.send_static_file('favicon.ico')
