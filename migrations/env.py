d3e3e5dab9297fc7120bac97dd04b025ea1b45f6
Thenu <anbunachiappan@gmail.com>
2023-09-18 16:18:03 -0700

    changed migration files and added

--- migrations/env.py
+++ migrations/env.py
@@ -2,11 +2,14 @@ from __future__ import with_statement
 
 import logging
 from logging.config import fileConfig
-
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 from flask import current_app
 
 from alembic import context
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
